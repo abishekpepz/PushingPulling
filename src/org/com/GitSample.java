cfb31105d0a26286491f607eadef3dcd3d045834
abishekpepz <102414705+abishekpepz@users.noreply.github.com>
2022-03-29 14:56:00 +0530

    method 4 (priyath)

--- src/org/com/GitSample.java
+++ src/org/com/GitSample.java
@@ -4,8 +4,19 @@ public class GitSample {
 	public void method1() {
        System.out.println("pushing");
 }
+		public void method2() {
+       System.out.println("pushing");
+}
+
+			public void method3() {
+       System.out.println("pushing");
+}
+public void method4() {
+       System.out.println("pushing");
+}
 	public static void main(String[] args) {
 		GitSample g = new GitSample();
 		g.method1();
 	}
+	
 }
